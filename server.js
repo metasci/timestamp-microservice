9781a2c6e9bf14c6342b6277681abfe9d1a72c5a
Nick Beaudoin <believewithyoureyes@gmail.com>
2016-06-09 19:15:39 +0000

    downsize - remove index.js

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+app.get('/', function(req, res) {
+	res.sendFile(process.cwd() + '/public/index.html');
+});
 
-app.use(passport.initialize());
-app.use(passport.session());
 
-routes(app, passport);
+
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
