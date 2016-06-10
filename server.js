6ee56622674862f25c0a7f4320d1a36df21ab6b3
Nick Beaudoin <believewithyoureyes@gmail.com>
2016-06-10 04:23:20 +0000

    add moment for date manipulation

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var moment = require('moment');
 var express = require('express');
+
 
 var app = express();
 
@@ -10,6 +12,20 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function(req, res) {
 	res.sendFile(process.cwd() + '/public/index.html');
 });
+
+
+
+app.get('/:datestring', function(req, res){
+	var theDate;
+	if(/^\d{8,}$/.test(req.params.datestring)){
+		theDate = moment
+	}
+});
+
+
+
+
+
 
 
 
